98e1152fb6e3cb33eb13f8ed787c452ad694213e
Tanner Hobson <thobson2@vols.utk.edu>
2018-04-11 12:58:35 -0400

    Try new build order

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3.6
 
 from distutils.core import setup, Extension
+from distutils.command.build import build as _build
 from pathlib import Path
+
+
+class build(_build):
+	sub_commands = [
+		('build_ext', _build.has_ext_modules),
+		('build_py', _build.has_pure_modules),
+		('build_clib', _build.has_c_libraries),
+		('build_scripts', _build.has_scripts),
+	]
 
 
 pyospray_module = Extension(
@@ -13,6 +23,7 @@ pyospray_module = Extension(
 
 
 setup(
+	cmdclass={'build': build},
 	name='pyospray',
 	packages=['pyospray'],
 	version='0.1.0',
